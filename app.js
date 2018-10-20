592362c8e692cbf7aa6f86984b1135c03b60cb1d
CoreyDickerson10 <tirsogarcia07@gmail.com>
2018-10-20 11:11:10 -0500

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // TODO: Enter the path to your service account json file
 // Need help with this step go here: https://firebase.google.com/docs/admin/setup
-const serviceAccount = require("./REPLACE_WITH_SERVICE_ACCOUNT.json");
+const serviceAccount = require("./maddencfm-cddb0-firebase-adminsdk-0ds5s-3512538fb9.json");
 
 // TODO: Enter your database url from firebase
 admin.initializeApp({
